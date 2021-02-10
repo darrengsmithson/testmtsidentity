e5ca64559d5de7530de9b6852fb74ee597d6e27f
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-02-10 14:15:07 +0000

    use token

--- testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
+++ testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
@@ -1,10 +1,19 @@
 package com.mts.identity.testrequest.testrequest.service;
 
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import org.springframework.http.MediaType;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping(path = "/testrequest")
@@ -20,6 +29,7 @@ public class RequestService {
         String uri = "/testresponse";
         String resp = getWebClient().get()
             .uri(uri)
+            .headers(h -> h.setBasicAuth(getClientSecret("")))
             .accept(MediaType.TEXT_PLAIN)
             .retrieve()
             .bodyToMono(String.class)
@@ -31,6 +41,21 @@ public class RequestService {
         WebClient wc = WebClient.create("https://testrespond-1612865660384.azurewebsites.net");
         return wc;
     }
+
+    private String getClientSecret() {
+
+        String key = null;
+
+        SecretClient secretClient = new SecretClientBuilder()
+                                        .vaultUrl("https://testreqkv.vault.azure.net/")
+                                        .credential(new DefaultAzureCredentialBuilder().build())
+                                        .buildClient();
+
+
+        return secretClient.getSecret("testreqsecret").getValue();
+
+    }
+
 
 
 }
