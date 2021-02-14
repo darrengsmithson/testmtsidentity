bfa4aaffe0d683e3be3d66592fe372922a1072dc
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-02-14 21:48:03 +0000

    Update RequestService.java

--- testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
+++ testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
@@ -27,7 +27,9 @@ public class RequestService {
     @GetMapping("/B")
     public String getResponseFromService() {
 
-        String token = getAzureToken();
+        //String token = getAzureToken();
+        String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85OTgwNDY1OS00MzFmLTQ4ZmEtODRjMS02NWM5NjA5ZGUwNWIvIiwiaWF0IjoxNjEzMzM1MjYxLCJuYmYiOjE2MTMzMzUyNjEsImV4cCI6MTYxMzMzOTE2MSwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhUQUFBQWpIRGxtVWpvSXAwL0tHOWpsK2RWa2tOc1pCbWg5QXZ5QlJVMCtRcTRBcFloYnoyOW1FbTJ6d3hpdmJDSUNid3ZLMmpueTR0VEtBc2duenVYdjUrUml3NzQ0b0ovT1Y4QVc5cFZPbm9uS3owPSIsImFtciI6WyJwd2QiLCJtZmEiXSwiYXBwaWQiOiJjNDRiNDA4My0zYmIwLTQ5YzEtYjQ3ZC05NzRlNTNjYmRmM2MiLCJhcHBpZGFjciI6IjIiLCJncm91cHMiOlsiZmE4ZDc1MWMtMTc0Ny00ZWRlLWI5MWMtYjliM2YyNjIwNmE0IiwiMTc2MmE4Y2EtNjc3OC00ZTNhLTk3MWUtZjA2NzFiMzNjOTE0Il0sImlwYWRkciI6IjgyLjI3LjE2Ny4xNSIsIm5hbWUiOiJEYXJyZW4iLCJvaWQiOiJhOGVmZWJjOS1hNGFmLTRjMjEtYTJlYy0zZGVmMDVkYWNiNmMiLCJwdWlkIjoiMTAwMzIwMDEwMjA1RkI5OSIsInJoIjoiMC5BQUFBV1VhQW1SOUQta2lFd1dYSllKM2dXNE5BUzhTd084Rkp0SDJYVGxQTDN6eDVBTE0uIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiMDF6TFlGZzZFTXVpalRhQjBaYllCNHA3c1VxQXFNdEw4cjZ1WDB5MWs5TSIsInRpZCI6Ijk5ODA0NjU5LTQzMWYtNDhmYS04NGMxLTY1Yzk2MDlkZTA1YiIsInVuaXF1ZV9uYW1lIjoiZGFycmVuLnNtaXRoc29uQG10c25kcGgub25taWNyb3NvZnQuY29tIiwidXBuIjoiZGFycmVuLnNtaXRoc29uQG10c25kcGgub25taWNyb3NvZnQuY29tIiwidXRpIjoib2ZJeXRqdDEzVVdWOE1fT2VLTmdBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiOWI4OTVkOTItMmNkMy00NGM3LTlkMDItYTZhYzJkNWVhNWMzIl0sInhtc190Y2R0IjoxNjA3NDQyNDQwfQ.eJjwuoWdLHitmNDc9lgfdvixmuWnnwwLntMlXsJSLZPm0icpBq0TT0b2fvYBioCLSF5cxj99Wxi9raAx5K2eHnnQuCM8aYTAVvbXVCfS4dpzvViqjUNsrTC4rdPl3ZG5qQRi83lyjIyxGDT8QuGC_YXQk23gKX4CC8LJirD134vA4vzpa_Ox6pr4l2MYS7hwgy1pNJAiytyCCq3JOYnEODNTt_Xx7OKDsT6H6rgd8YI7iwsBfXQy7vTUAOLe-z1Pj3ofG33esOnLku-bLPauqIYl5p9eOVWpiDqNokcrNZhDMnb0_UmWiwf3qZojYLvD9BNdOqiFE4KH_rEdqbgqIw";
+
 
         String uri = "/testresponse";
         String resp = getWebClient().get()
@@ -73,7 +75,7 @@ public class RequestService {
     @GetMapping("/testchange")
     public String getTestChange() {
 
-        return "test change 3";
+        return "test change 4";
     }
 
     public WebClient getWebClient() {
