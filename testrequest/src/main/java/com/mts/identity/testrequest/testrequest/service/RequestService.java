19d3b7ddcabf93e356c01057cd312ce139c9cefd
Darren Smithson <33603539+darrengsmithson@users.noreply.github.com>
2021-03-06 15:58:04 +0000

    update

--- testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
+++ testrequest/src/main/java/com/mts/identity/testrequest/testrequest/service/RequestService.java
@@ -44,51 +44,6 @@ public class RequestService {
         return resp + " from calling service B";
     }
 
-    @GetMapping("/C")
-    public String getResponseFromServiceUsingURL() throws Exception {
-
-        String token = getTokenFromURL();
-
-        String uri = "/testresponse";
-        String resp = getWebClient().get()
-            .uri(uri)
-            .headers(h -> h.setBearerAuth(token))
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
-        return resp + " from calling service B";
-    }
-
-    @GetMapping("/D")
-    public String getTokenFromService() {
-
-        String token = getAzureToken();
-
-        return "token from managed identity - " + token;
-    }
-
-    @GetMapping("/E")
-    public String getTokenFromServiceUsingURL() throws Exception {
-
-        String token = getTokenFromURL();
-
-        return "token from url - " + token;
-    }
-
-    @GetMapping("/F")
-    public String getTESTTokenFromServiceUsingURL() {
-
-        String token = getTestTokenFromURL();
-
-        return "TEST OUTPUT from url - " + token;
-    }
-
-    @GetMapping("/testchange")
-    public String getTestChange() {
-
-        return "test change 18";
-    }
-
     public WebClient getWebClient() {
         WebClient wc = WebClient.create("https://testrespond-1612865660384.azurewebsites.net");
         return wc;
@@ -104,56 +59,6 @@ public class RequestService {
 
         return x.getToken();
 
-    }
-
-    private String getTokenFromURL() throws Exception {
-
-        try
-        {
-
-            URL url = new URL("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2019-08-01&resource=https://management.azure.com/");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Metadata", "true");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while ((line = in.readLine()) != null) {
-                response.append(line);
-            }
-            in.close();
-            return response.toString();
-        }
-        catch(Exception ex)
-        {
-
-            return "ex is -  " + ex.getMessage();
-        }
-
-    }
-
-    private String getTestTokenFromURL()
-    {
-        try
-        {
-
-            URL url = new URL("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=2b1fd2d7f77ccf1b7de9b441571b39b8");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("Metadata", "true");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while ((line = in.readLine()) != null) {
-                response.append(line);
-            }
-            in.close();
-            return response.toString();
-        }
-        catch(Exception ex)
-        {
-            return "ex is -  " + ex.getMessage();
-        }
     }
 
 }
--- testrespond/src/main/java/com/mts/identity/testrespond/testrespond/service/RespondService.java
+++ testrespond/src/main/java/com/mts/identity/testrespond/testrespond/service/RespondService.java
@@ -3,6 +3,7 @@ package com.mts.identity.testrespond.testrespond.service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping(path = "/testresponse")
@@ -11,6 +12,27 @@ public class RespondService {
     @GetMapping
     public String getResponse() {
         return "from service B";
+    }
+
+    @GetMapping("/A")
+    public String getResponseFromService() throws Exception {
+
+        //msiks();
+        //Mono<AccessToken> token = getAzureToken();
+
+        String uri = "/testrequest";
+        String resp = getWebClient().get()
+            .uri(uri)
+            //.headers(h -> h.setBearerAuth(token.block().getToken()))
+            .retrieve()
+            .bodyToMono(String.class)
+            .block();
+        return resp + " from calling service A";
+    }
+
+    public WebClient getWebClient() {
+        WebClient wc = WebClient.create("https://testrequest-1612878461242.azurewebsites.net");
+        return wc;
     }
 
 
